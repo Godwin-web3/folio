d8f417c3f4b9281c0f083c14c6528a57557fff27
GodwinXbt <mbagodwin419@gmail.com>
2026-09-06 11:54:56 +0000

    fix(auth): harden Convex Auth session and login UX

--- spa/root.tsx
+++ spa/root.tsx
@@ -1,14 +1,11 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { AuthProvider } from "@/lib/auth/provider";
-import { FolioConvex } from "@/lib/open-address/convex-react";
 
 function Shell() {
   return (
-    <FolioConvex>
-      <AuthProvider>
-        <Outlet />
-      </AuthProvider>
-    </FolioConvex>
+    <AuthProvider>
+      <Outlet />
+    </AuthProvider>
   );
 }
 
